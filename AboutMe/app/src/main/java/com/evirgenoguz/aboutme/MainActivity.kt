078e3cffe30c9a9eedce57cab40bf332a23c1526
Oguz Evirgen <evirgenoguz@gmail.com>
2022-01-07 17:46:06 +0300

    databinding added

--- AboutMe/app/src/main/java/com/evirgenoguz/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/evirgenoguz/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.evirgenoguz.aboutme
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,16 +9,24 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.evirgenoguz.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val myName: MyName = MyName("Oguz Evirgen")
 
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
             updateNickname(it)
         }
+
 
     }
 
@@ -25,10 +34,16 @@ class MainActivity : AppCompatActivity() {
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
+            invalidateAll()
+        }
+
+
 
         // Hide the keyboard.
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ AboutMe/app/src/main/java/com/evirgenoguz/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package com.evirgenoguz.aboutme
+
+data class MyName(var name: String = "", var nickname: String = "")
